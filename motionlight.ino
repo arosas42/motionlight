c3bb97b0b339b531ed019172d377a53bb592a1e9
Arturo <arturo@r03ajrhc8.managed.mst.edu>
2017-03-29 10:26:49 -0500

    Improvements
    * tweaked PWM_rollover
    * merged STATE_TRANSIT, STATE_HIGH logic in state machine
    * fixed state typo DAYLIGHT -> STATE_DAYLIGHT
    * STATE_DAYLIGHT now turns off PWM interrupts during daytime

--- motionlight.ino
+++ motionlight.ino
@@ -6,7 +6,7 @@ byte low  = 2;
 byte high = 2047;
 
 // pwm controls
-#define PWM_rollover  2048
+#define PWM_rollover  2047
 #define PWM_delay     127
 unsigned int PWM_dc = low;  
 
@@ -44,6 +44,7 @@ void loop()
   static unsigned long old_seconds = cur_seconds;
   boolean motion = (PINB & MOTION_PIN);
   static int lightValue = analogRead(0);
+  int new_dc;
 
   if ( cur_seconds - old_seconds > 5 )
   {
@@ -69,20 +70,16 @@ void loop()
       break;
       
     case STATE_TRANSIT:
-      if ( lightValue >= DAYLIGHT )
-      {
-        cur_state = STATE_DAYLIGHT;
-      }
-      else if ( PWM_dc >= high )
+      new_dc = millis_to_dc( cur_millis - start );
+      if ( new_dc >= high )
         cur_state = STATE_HIGH;
       else
-        PWM_set_dc( millis_to_dc( cur_millis - start ) );
-      break;
-      
+        PWM_set_dc( new_dc );
+
     case STATE_HIGH:
       if ( lightValue >= DAYLIGHT )
       {
-        cur_state = DAYLIGHT;
+        cur_state = STATE_DAYLIGHT;
       }
       else if ( (cur_seconds - then) > interval )
       {
@@ -95,10 +92,14 @@ void loop()
       if ( lightValue <= NIGHTLIGHT )
       {
         cur_state = STATE_LOW;
+        PWM_setup();
         PWM_set_dc( low );
       }
-      else
-        PWM_set_dc(0);
+      else if ( PWM_dc > 0 ) // shutdown PWM functions only once
+      {
+        PWM_set_dc( 0 );
+        PWM_shutdown();
+      }
       break;
   }
   
@@ -129,6 +130,15 @@ void PWM_setup()
   TIMSK  |= _BV(OCIE0A);  // enable interrupt for compare match A   
   TCCR0A  = _BV(WGM01);   // CTC mode
   TCCR0B  = _BV(CS01);    // clk/8 prescaling
+  sei();
+}
+
+void PWM_shutdown()
+{
+  cli();
+  TCNT0  = 0;              // reset counter
+  TIMSK &= ~(_BV(OCIE0A)); // disable interrupt
+  PORTB &= ~LED_PIN;       // turn off LED
   sei();
 }
 
